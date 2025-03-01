9c2651b7bfcf5005e0bd520c06c07e1921f064e3
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2025-03-01 23:32:55 +0100

    Added all folder structure according to project plan and created all  files

---
+++ src/components/CartIcon.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CartIcon extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ src/pages/CheckoutSuccessPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CheckoutSuccessPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
